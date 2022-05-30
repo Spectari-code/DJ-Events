b5db25ac8700af51e199cf9fa18443ad302a9d73
Spectari-Code <spectarivoid@gmail.com>
2022-05-30 20:48:44 +0200

    Layout, Header and Footer.

--- dj-events-frontend/components/Layout.js
+++ dj-events-frontend/components/Layout.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Header from "./Header";
+import Footer from "./Footer";
 import styles from "../styles/Layout.module.css";
 
 // pass children as props because Layout components wraps all the page's content
@@ -11,7 +13,9 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="keywords" content={keywords} />
       </Head>
 
+      <Header />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
