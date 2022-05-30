396e6b6b23dc92630dc640290e0c53f287261c5f
Spectari-Code <spectarivoid@gmail.com>
2022-05-30 14:09:58 +0200

    Added
    Added files.

---
+++ dj-events-frontend/pages/_app.js
@@ -0,0 +1,10 @@
+// MyApp component initializes pages
+
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  // Returns whatever page I'm at, for example if I'm in home page it returns the Home component
+  return <Component {...pageProps} />
+}
+
+export default MyApp
